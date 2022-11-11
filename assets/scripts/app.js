9c4f07bb81181b6ba8e33645917c08d36daa1252
Have Samuel <samiehave9@gmail.com>
2022-11-11 21:30:07 +0300

    Using Constants

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,7 +1,8 @@
-let currentResult = 0;
+const defaultResult = 0;
+let currentResult = defaultResult;
 
 currentResult += (currentResult + 10) * 3 / 2 - 1;
 
-let currentDescription = '(' + currentResult + ' + 10) * 3 / 2 - 1';
+let currentDescription = '(' + defaultResult + ' + 10) * 3 / 2 - 1';
 
 outputResult(currentResult, currentDescription);
