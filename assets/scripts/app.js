636f51b3dd2a06ae6d995ee441d2d58458c18898
Have Samuel <samiehave9@gmail.com>
2022-11-13 21:29:51 +0300

    Refactoring the code to make sure our Unconventional Calculator works

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -5,11 +5,39 @@ function getUserNumberInput() {
   return parseInt(usrInput.value);
 }
 
-function add() {
-  const enteredNumber = getUserNumberInput();
-  const calDescription = `${currentResult} + ${enteredNumber}`;
-  currentResult += currentResult + enteredNumber;
+function createAndWriteOutput(operator, resultBeforeCal, calNumber) {
+  const calDescription = `${resultBeforeCal} ${operator} ${calNumber}`;
   outputResult(currentResult, calDescription);
 }
 
+function add() {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult = currentResult + enteredNumber;
+  createAndWriteOutput('+', initialResult, enteredNumber);
+}
+
+function subtract() {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult = currentResult - enteredNumber;
+  createAndWriteOutput('-', initialResult, enteredNumber);
+}
+
+function multiply() {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult = currentResult * enteredNumber;
+  createAndWriteOutput('*', initialResult, enteredNumber);
+}
+
+function divide() {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult = currentResult / enteredNumber;
+  createAndWriteOutput('/', initialResult, enteredNumber);
+}
 addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', subtract);
+divideBtn.addEventListener('click', divide);
+multiplyBtn.addEventListener('click', multiply);
